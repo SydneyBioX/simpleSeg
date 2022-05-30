38e36c439da52116b0f73de98e08ef714cb7ede5
EllisPatrick <ellis.patrick@sydney.edu.au>
2022-05-30 14:01:29 +1000

    Initiate package

---
+++ R/simpleSeg.R
@@ -0,0 +1,18 @@
+#' Perform simple segmentation
+#'
+#' @param image An image
+#' @param BPPARAM A BiocParallelParam object.
+#'
+#' @return A list of image masks
+#'
+#' @examples
+#' 
+#' 1+1
+#'
+#' @export
+#' @rdname simpleSeg
+#' @importFrom BiocParallel SerialParam bplapply
+simpleSeg <-
+  function(image){
+    1+1
+  }
