6e682c29ed2c2d32e97912ccb97e428c9fd22cdc
Alexander Nicholls <alexanderwnicholls@gmail.com>
2022-06-14 15:28:10 +1000

    Using bpmapply instead of mcmapply Simpleseg function moved to the top of the file NormalizeCells moved to it's own function

--- R/simpleSeg.R
+++ R/simpleSeg.R
@@ -12,9 +12,9 @@
 #' @param autosmooth dynamically scales smoothing based on signal to noise ratio of individual images
 #' @param tolerance
 #' @param ext = 1,
-#' @param kernSize size of dilation around nuclei to create cell disk
+#' @param kernSize size of dilation around nuclei to create cell disk #dilation size
 
-#' @param size_selectionCyt
+#' @param size_selectionCyt #sizeSelectionCyt
 #' @param minMax #scale image channel intensities between 0 and 1
 # asin = FALSE #perform asinh normalization on image channels
 
@@ -30,14 +30,98 @@
 #'
 #' @export
 #' @rdname simpleSeg
-#' @importFrom BiocParallel SerialParam bplapply MulticoreParam
+#' @importFrom BiocParallel SerialParam bplapply MulticoreParam pbmapply
 #' @importFrom EBImage gblur otsu bwlabel makeBrush filter2 watershed dilate distmap propagate
-#' @importFrom parallel mcmapply
 #' @importFrom terra predict
 #' @importFrom cytomapper CytoImageList
 #' @importFrom stats prcomp quantile lm 
 #' @importFrom BiocSingular RandomParam
 #' @importFrom BiocNeighbors AnnoyParam
+
+
+
+simpleSeg <- function(#nmask parameters
+    image,
+    cytIdentification = "dilate",
+    nucleus_index = 1,
+    size_selectionNuc = 10,
+    smooth = 1,
+    norm99 = TRUE,
+    maxThresh = TRUE,
+    autosmooth = TRUE,
+    tolerance = 0.01,
+    ext = 1,
+    kernSize = 3,
+    
+    #cyt1 parameters
+    #nmask,
+    
+    size_selectionCyt = 5,
+    minMax = FALSE,
+    asin = FALSE,
+    
+    #cyt2 parameters
+    #nmask,
+    cyt_index=2,
+    
+    cores = 50
+){
+    # do nmask (if cytIdentification is null return nuc mask)
+    whole_cell = FALSE
+    if (cytIdentification == "dilate"){
+        whole_cell = TRUE
+    }
+    
+    nmask <- nucSegParalell( image,
+                             nucleus_index = nucleus_index,
+                             size_selection = size_selectionNuc,
+                             smooth = smooth,
+                             norm99 = norm99,
+                             maxThresh = maxThresh,
+                             autosmooth = autosmooth,
+                             tolerance = tolerance,
+                             ext = ext,
+                             whole_cell = whole_cell,
+                             kernSize = kernSize,
+                             cores = cores)
+    
+    #if dilate
+    if (cytIdentification == "dilate"){
+        return(CytoImageList(nmask))
+    }
+    
+    if (cytIdentification == "diskModel"){
+        
+        cells <- cytSegParalell (nmask,
+                                 image,
+                                 size_selection = size_selectionCyt,
+                                 smooth = smooth,
+                                 kernSize = kernSize,
+                                 minMax = minMax,
+                                 asin = asin,
+                                 cores = cores)
+        
+        return(CytoImageList(cells))
+    }
+    
+    #if marker
+    else if (cytIdentification == "markerModel"){
+        
+        cells <- cytSeg2Paralell(nmask,
+                                 image,
+                                 channel = cyt_index,
+                                 size_selection = size_selectionCyt,
+                                 smooth = smooth,
+                                 minMax = minMax,
+                                 asin = asin,
+                                 cores = cores)
+        
+        return(CytoImageList(cells))
+    }
+}
+
+
+
 
 autosmooth <- function(channel, smooth, threshold, adjustment){
     signal <- mean(channel[,,9])
@@ -89,7 +173,7 @@ nucSeg <- function(image,
     nuc1[is.na(nuc)] <- 0
     
     
-    nuc1 <- gblur(sqrt(nuc1),
+    nuc1 <- EBImage::gblur(sqrt(nuc1),
                   smooth)
     
     
@@ -247,7 +331,7 @@ cytSegParalell <- function(nmask,
                            minMax = FALSE,
                            asin = FALSE,
                            cores = 50){
-    test.masks.cyt <- mcmapply(CytSeg, nmask, image, size_selection = size_selection, smooth = smooth, kernSize = kernSize, minMax = minMax, asin = asin, mc.cores = cores)
+    test.masks.cyt <- BiocParallel::bpmapply(CytSeg, nmask, image, size_selection = size_selection, smooth = smooth, kernSize = kernSize, minMax = minMax, asin = asin, BPPARAM  = BiocParallel::MulticoreParam(workers = cores))
 }
 
 
@@ -308,188 +392,19 @@ cytSeg2Paralell <- function(nmask,
                             minMax = FALSE,
                             asin = FALSE,
                             cores = 50){
-    test.masks.cyt <- mcmapply(CytSeg2, nmask, image, channel = channel, size_selection = size_selection, smooth = smooth,  mc.cores = 40)
+    test.masks.cyt <- BiocParallel::bpmapply(CytSeg2, nmask, image, channel = channel, size_selection = size_selection, smooth = smooth,  BPPARAM  = BiocParallel::MulticoreParam(workers = cores))
 }
+# bpmapply()
 
 
 ### Simple seg
-simpleSeg <- function(#nmask parameters
-    image,
-    cytIdentification = "dilate",
-    nucleus_index = 1,
-    size_selectionNuc = 10,
-    smooth = 1,
-    norm99 = TRUE,
-    maxThresh = TRUE,
-    autosmooth = TRUE,
-    tolerance = 0.01,
-    ext = 1,
-    kernSize = 3,
-    
-    #cyt1 parameters
-    #nmask,
-    
-    size_selectionCyt = 5,
-    minMax = FALSE,
-    asin = FALSE,
-    
-    #cyt2 parameters
-    #nmask,
-    cyt_index=2,
-    
-    cores = 50
-){
-    # do nmask (if cytIdentification is null return nuc mask)
-    whole_cell = FALSE
-    if (cytIdentification == "dilate"){
-        whole_cell = TRUE
-    }
-    
-    nmask <- nucSegParalell( image,
-                             nucleus_index = nucleus_index,
-                             size_selection = size_selectionNuc,
-                             smooth = smooth,
-                             norm99 = norm99,
-                             maxThresh = maxThresh,
-                             autosmooth = autosmooth,
-                             tolerance = tolerance,
-                             ext = ext,
-                             whole_cell = whole_cell,
-                             kernSize = kernSize,
-                             cores = cores)
-    
-    #if dilate
-    if (cytIdentification == "dilate"){
-        return(CytoImageList(nmask))
-    }
-    
-    if (cytIdentification == "diskModel"){
-        
-        cells <- cytSegParalell (nmask,
-                                 image,
-                                 size_selection = size_selectionCyt,
-                                 smooth = smooth,
-                                 kernSize = kernSize,
-                                 minMax = minMax,
-                                 asin = asin,
-                                 cores = cores)
-        
-        return(CytoImageList(cells))
-    }
-    
-    #if marker
-    else if (cytIdentification == "markerModel"){
-        
-        cells <- cytSeg2Paralell(nmask,
-                                 image,
-                                 channel = cyt_index,
-                                 size_selection = size_selectionCyt,
-                                 smooth = smooth,
-                                 minMax = minMax,
-                                 asin = asin,
-                                 cores = cores)
-        
-        return(CytoImageList(cells))
-    }
-}
 
 
 
 
 
 
-normalize.cells <- function(cells,
-                            markers,
-                            isSCE = FALSE,
-                            assayName = NULL,
-                            imageNb = NULL, #requires the image numbering varlable from sce list data to stratify the cells on
-                            transformation = NULL,
-                            method = NULL){
-    
-    #handeling sce and se
-    if (isSCE == TRUE){
-        if (is.null(assayName)){
-            cellsdf <- data.frame(t(assay(cells)))
-        }
-        else{
-            cellsdf <- data.frame(t(cells@assays@data@listData[[assayName]]))
-        }
-        cellsdf$imageID <- cells@colData@listData[[imageNb]]
-        cells <- cellsdf
-    }
-    
-    #Transformations
-    if (transformation == "asinh"){
-        cells[,markers] <- asinh(cells[,markers])
-    }
-    if (transformation == "sqrt"){
-        cells[,markers] <- sqrt(cells[,markers])
-    }
-    if (transformation == "log"){
-        cells[,markers] <- log10(cells[,markers])
-    }
-    #Methods
-    if (method == "meandiv"){
-        for(i in 1:length(unique(cells$imageID))){
-            cells[cells$imageID==i,markers] <- sweep(cells[cells$imageID==i,markers], 2, apply(cells[cells$imageID==i,markers],2,mean, 0.2), "/")
-        }
-    }
-    if (method == "99perc"){
-        cells[,markers] <- data.frame(apply(cells[,markers],
-                                            2, 
-                                            function(x){
-                                                q <- quantile(x,0.99)
-                                                pmin(x,q)/q
-                                            }))
-    }
-    if (method == "1stPC"){
-        pca <- prcomp(cells[,markers])
-        
-        PC1 <- pca$x[,"PC1"]
-        
-        
-        for(i in markers){
-            y <- cells[,i]
-            fit <- lm(y~PC1)
-            int <- fit$coefficients["(Intercept)"]
-            cells[,i] <- pmax(resid(fit) + int,0)
-            q <- quantile(cells[,i],0.99)
-            cells[,i] <- pmin(cells[,i],q)/q
-        }
-        
-        
-    }
-    if (method == "scMerge"){
-        #sc merge
-        ncores <- 64
-        use_bpparam <- BiocParallel::MulticoreParam(workers = ncores)
-        use_bsparam <- BiocSingular::RandomParam()
-        use_bnparam <- BiocNeighbors::AnnoyParam()
-        # dat_sub <- dat[sample(nrow(dat), 500000), ]
-        # ctl_genes <- rownames(sce)
-        exprsMat <- t(cells[, markers])
-        colnames(exprsMat) <- seq_len(ncol(exprsMat))
-        scMerge_res <- scMerge2(exprsMat = exprsMat, #the exprs matrix to be normalised
-                                batch = cells$imageID, # batch labels
-                                cellTypes = NULL, # set NULL clustering will be performed within scMerge2... can also try the published cell type labels, which will match between the cell types
-                                use_bpparam = use_bpparam,
-                                use_bsparam = use_bsparam,
-                                use_bnparam = use_bnparam,
-                                ruvK = 2, # Number of unwanted variation to be removed
-                                ctl = markers, # negative control genes
-                                k_psuedoBulk = 5, # Number of pseudo bulk to be created for each cell type each batch
-                                k_celltype = 20, # Number of neighbours when perform graph clustering
-                                pseudoBulk_fn = create_pseudoBulk, # ways of constructing pseudo bulk
-                                ncores = ncores,
-                                chosen.hvg = markers, #Highly variable genes to be used to identify pseudo-replicates... since IMC has very few features, using all features.
-                                cosineNorm = F,
-                                return_subset = FALSE,
-                                normalised = T)
-        dat_norm <- as.data.frame(scMerge_res$newY)
-        cells[, markers] <- dat_norm
-    }
-    return(cells)
-}
+
 
 
 
